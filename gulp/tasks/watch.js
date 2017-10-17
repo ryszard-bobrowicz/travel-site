1fff8ef030d8901728f58724581e3925c505399b
Ryszard Bobrowicz <ryszard.bobrowicz@gmail.com>
2017-10-17 21:08:21 +0200

    v. 1.0.1

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -21,8 +21,7 @@ gulp.task('watch', function() {
 
 });
 
-
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
-  .pipe(browserSync.stream());
+    .pipe(browserSync.stream());
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,13 @@
-require('./gulp/tasks/styles.js');
-require('./gulp/tasks/watch.js');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+
+
+
+
+
+
+
+
+
